ccb54da64ee27175b8f2b8ce22aa19ed3b3e8ac3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 08:02:21 -0300

    grid e flexbox

---
+++ grid/src/Pages/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+        <header className="flex bg-[#87CEFA] p-4 mb-4">
+            <div className="flex items-center space-x-2">
+            </div>
+            <Navbar />
+        </header>
+    )
+
+}
